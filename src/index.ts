d9642c0086f00496f7d4f8068b60647f7d038c95
unknown <amutuyimana64@gmail.com>
2026-04-30 09:11:07 +0200

    html tamplate

--- src/index.ts
+++ src/index.ts
@@ -1,5 +1,5 @@
 import "dotenv/config";
-import express from 'express';
+import express, { Request, Response } from 'express';
 import usersRouter from './routes/users.routes';
 import authRouter from './routes/auth.routes';
 import listingsRouter from './routes/listings.routes';
@@ -8,6 +8,7 @@ import { connectDb } from "./config/prisma";
 import { errorHandler } from "./middlewares/errorHandler";
 import uploadRouter from "./routes/upload.routes"
 import { setupSwagger } from "./config/swagger";
+import path from "path";
 const app = express();
 const PORT = process.env.PORT || 8080;
 
@@ -18,6 +19,10 @@ app.use("/users",uploadRouter)
 app.use('/listings',listingsRouter);
 app.use('/auth',authRouter);
 app.use('/bookings',bookingsRouter);
+app.get('/', (req: Request, res: Response) => {
+    // Send the actual HTML file
+    res.sendFile(path.join(__dirname, '../public/index.html'));
+});
 
 app.use(errorHandler)
 // Start the server
